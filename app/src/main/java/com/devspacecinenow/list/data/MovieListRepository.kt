fa74109ff37385bbac09dc71a06e1bf99fb8bb0c
calculo-combustivel <miguelingoschaade@gmail.com>
2025-02-13 20:37:37 -0300

    offline first

--- app/src/main/java/com/devspacecinenow/list/data/MovieListRepository.kt
+++ app/src/main/java/com/devspacecinenow/list/data/MovieListRepository.kt
@@ -35,15 +35,15 @@ class MovieListRepository(
 
     suspend fun getTopRated(): Result<List<Movie>?> {
         return try {
-            val result = remote.getNowPlaying()
+            val result = remote.getTopRated()
             if (result.isSuccess) {
                 val moviesRemote = result.getOrNull() ?: emptyList()
                 if (moviesRemote.isNotEmpty()) {
                     local.updateLocalItems(moviesRemote)
                 }
-                return Result.success(local.getNowPlayingMovies())
+                return Result.success(local.getTopRatedMovies())
             } else {
-                val localData = local.getNowPlayingMovies()
+                val localData = local.getTopRatedMovies()
                 if (localData.isEmpty()) {
                     return result
                 } else {
@@ -58,15 +58,15 @@ class MovieListRepository(
 
     suspend fun getPopular(): Result<List<Movie>?> {
         return try {
-            val result = remote.getNowPlaying()
+            val result = remote.getPopular()
             if (result.isSuccess) {
                 val moviesRemote = result.getOrNull() ?: emptyList()
                 if (moviesRemote.isNotEmpty()) {
                     local.updateLocalItems(moviesRemote)
                 }
-                return Result.success(local.getNowPlayingMovies())
+                return Result.success(local.getPopularMovies())
             } else {
-                val localData = local.getNowPlayingMovies()
+                val localData = local.getPopularMovies()
                 if (localData.isEmpty()) {
                     return result
                 } else {
@@ -81,15 +81,15 @@ class MovieListRepository(
 
     suspend fun getUpComing(): Result<List<Movie>?> {
         return try {
-            val result = remote.getNowPlaying()
+            val result = remote.getUpComing()
             if (result.isSuccess) {
                 val moviesRemote = result.getOrNull() ?: emptyList()
                 if (moviesRemote.isNotEmpty()) {
                     local.updateLocalItems(moviesRemote)
                 }
-                return Result.success(local.getNowPlayingMovies())
+                return Result.success(local.getUpcomingMovies())
             } else {
-                val localData = local.getNowPlayingMovies()
+                val localData = local.getUpcomingMovies()
                 if (localData.isEmpty()) {
                     return result
                 } else {
